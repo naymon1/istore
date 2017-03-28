8553d5edcc97767630692d28c6192ce94bf62b1b
naymon1 <nnamdiijeh@yahoo.com>
2017-03-28 11:31:00 +0100

    ths include file

--- www/test.php
+++ www/test.php
@@ -7,7 +7,7 @@ define ('DBPASS', 'naymon');
 try{
 	#prepare a pdo instance
 
-    $pdo = new PDO('mysql:host=localhost; dbname='"test", "root","naymon");
+    $pdo = new PDO('mysql:host=localhost; dbname='"test.php", "root","naymon");
 
     # set verbose error modes
     $dbh-setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_SILENT);
