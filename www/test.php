40225f5ea779742747f322d022ab6ec2d7dbe048
naymon1 <nnamdiijeh@yahoo.com>
2017-03-28 11:17:16 +0100

    pdo data connection

--- www/test.php
+++ www/test.php
@@ -1,3 +1,19 @@
 <?php 
-$pdo = new PDO('mysql:host=localhost; dbname=istore', "root","naymon");
+
+define ('DBNAME', 'test');
+define ('DBUSER', 'root');
+define ('DBPASS', 'naymon');
+
+try{
+	#prepare a pdo instance
+
+    $pdo = new PDO('mysql:host=localhost; dbname='"test", "root","naymon");
+
+    # set verbose error modes
+    $dbh-setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_SILENT);
+
+} catch(PDOException $e){
+	echo $e->getMessage();
+}
+
 ?>
